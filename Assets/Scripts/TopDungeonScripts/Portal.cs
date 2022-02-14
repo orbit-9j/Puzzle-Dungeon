62ac5da44d9ab61302805f5b237ba5a71ce6db00
ur mom <tiana1@cardiff.ac.uk>
2022-02-14 13:14:29 +0000

    can move onto the next level

--- Assets/Scripts/TopDungeonScripts/Portal.cs
+++ Assets/Scripts/TopDungeonScripts/Portal.cs
@@ -11,8 +11,9 @@ public class Portal : Collidable
         if (coll.name == "Player")
         {
             GameManager.instance.SaveState();
-            string sceneName = sceneNames[Random.Range(0, sceneNames.Length)];
-            SceneManager.LoadScene(sceneName);
+            /*string sceneName = sceneNames[Random.Range(0, sceneNames.Length)];
+            SceneManager.LoadScene(sceneName); */
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
         }
     }
 }
